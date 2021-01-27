ca1a0046bbb9c8ce417de5f93b2a43d3424edb69
Morley Tatro <morleytatro@gmail.com>
2021-01-26 17:26:39 -0800

    spruced up the CSS a bit

--- src/components/Container.js
+++ src/components/Container.js
@@ -11,9 +11,13 @@ function Container() {
 
   return (
     <div className={'container ' + (darkMode ? ' dark' : 'light')}>
-      <ToggleButton enabled={darkMode} handleClick={toggleDarkMode}/>
-      <ToggleButton enabled={darkMode} handleClick={toggleDarkMode}/>
-      <ToggleButton enabled={darkMode} handleClick={toggleDarkMode}/>
+      <div className="flex">
+        <ToggleButton enabled={darkMode} handleClick={toggleDarkMode}/>
+        <ToggleButton enabled={darkMode} handleClick={toggleDarkMode}/>
+        <ToggleButton enabled={darkMode} handleClick={toggleDarkMode}/>
+      </div>
+      <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eius cupiditate quos illum dolorem necessitatibus expedita ad quisquam magni, modi maxime deserunt obcaecati, nisi natus placeat mollitia quas quod recusandae!</p>
+      <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eius cupiditate quos illum dolorem necessitatibus expedita ad quisquam magni, modi maxime deserunt obcaecati, nisi natus placeat mollitia quas quod recusandae!</p>
     </div>
   )
 }
